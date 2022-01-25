cc31ce099a62b11936cf4dbcc0fe00cd8e230b55
Vladimir Usov <vlumailbox@gmail.com>
2022-01-25 23:01:38 +0300

    Outlets and Actions added on ViewController. Sliders actions connect to corresponding labels

--- ColorTintSlider/ViewController.swift
+++ ColorTintSlider/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var colorPalette: UIView!
+    
+    @IBOutlet weak var redValue: UILabel!
+    @IBOutlet weak var greenValue: UILabel!
+    @IBOutlet weak var blueValue: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorPalette.layer.cornerRadius = 10
     }
 
-
+    
+    @IBAction func changeRedColor() {
+        redValue.text = String(round(redSlider.value * 100) / 100)
+        
+    }
+    
+    @IBAction func changeGreenColor() {
+        greenValue.text = String(round(greenSlider.value * 100) / 100)
+        
+    }
+    
+    
+    @IBAction func changeBlueColor() {
+        blueValue.text = String(round(blueSlider.value * 100) / 100)
+        
+    }
 }
+
 
